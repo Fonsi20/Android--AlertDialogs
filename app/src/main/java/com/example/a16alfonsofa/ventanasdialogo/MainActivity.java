a0e68563a327645b2d0777cc90decb85d8e424bf
a16alfonsofa <fonsi20fernandez@gmail.com>
2019-01-25 14:13:52 +0100

    Segundo Botton Main  añadido. - Ventana de Dialog con un botón para salir y impidiendo que se salga del dialog a no ser que pinches en el boton.

--- app/src/main/java/com/example/a16alfonsofa/ventanasdialogo/MainActivity.java
+++ app/src/main/java/com/example/a16alfonsofa/ventanasdialogo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a16alfonsofa.ventanasdialogo;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,7 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnBuilder;
+    private Button btnBuilder,btnBuilderBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnBuilder = (Button) findViewById(R.id.btnBuilderDialog);
+        btnBuilderBtn = (Button) findViewById(R.id.btnBuilderDialogBOTON);
 
 
         btnBuilder.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +29,26 @@ public class MainActivity extends AppCompatActivity {
                 ventana.setMessage("Cuidado! hay terribles velocirraptores sueltos por el complejo, por favor no olvide no darles de comer.");
                 ventana.setIcon(android.R.drawable.star_big_on);
                 ventana.show();
+
+            }
+        });
+
+        btnBuilderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder ventana2 = new AlertDialog.Builder(MainActivity.this);
+                ventana2.setTitle("TE QUIERO");
+                ventana2.setMessage("Se que eres una persona preciosa, así que bésame y no me sueltes nunca!.");
+                ventana2.setIcon(android.R.drawable.ic_menu_myplaces);
+                ventana2.setCancelable(false);
+                ventana2.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                ventana2.show();
 
             }
         });
